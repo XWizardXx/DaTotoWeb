ad5e9f7ef04a60d30fcb3dd1ceba2f0b3e44d761
CarlettoDeiSoffocotti <blackgamer034@gmail.com>
2024-07-07 11:25:46 +0100

    spring initialization

---
+++ src/main/java/org/example/datotoweb/DaTotoWebApplication.java
@@ -0,0 +1,15 @@
+package org.example.datotoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaTotoWebApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DaTotoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datotoweb/DaTotoWebApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.datotoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaTotoWebApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
